7aba1342ea581d5675e674fa448d57239df17d85
deadlyunicorn <hello@deadlyunicorn.dev>
2025-07-11 14:30:29 +0800

    dateUtils -> timeUtils and moved releveant methods there from the general utils

--- application/lib/utils/index.ts
+++ application/lib/utils/index.ts
@@ -1,58 +1,21 @@
 import prettyMs from "pretty-ms";
-import { DateUtils } from "./dateUtils";
+import { TimeUtils } from "./timeUtils";
 import { AlertType, IAlert } from "@/components/AlertListener/interface";
 import { Events } from "../consts";
 
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace Utils {
-  export const calculateDuration = (start: string, end: string): number => {
-    const startDate = new Date(`2000-01-01T${start}`);
-    const endDate = new Date(`2000-01-01T${end}`);
-    return Math.round((endDate.getTime() - startDate.getTime()) / (1000 * 60));
-  };
+  // export const calculateDuration = (start: number, end: number): number => {
+  //   const startDate = new Date(`2000-01-01T${start}`);
+  //   const endDate = new Date(`2000-01-01T${end}`);
+  //   return Math.round((endDate.getTime() - startDate.getTime()) / (1000 * 60));
+  // };
 
-  export const formatDuration = (minutes: number): string => {
-    const hours = Math.floor(minutes / 60);
-    const mins = minutes % 60;
-    return `${hours}h ${mins}m`;
-  };
-
-  export const dateToInputValue = (date: Date): string => {
-    return date.toISOString().split("T")[0];
-  };
-
-  const getDateAttributes = (date: Date) => {
-    const [dayOfWeek, month, dayOfMonth, year] = date.toDateString().split(" ");
-
-    return {
-      dayOfWeek,
-      month,
-      dayOfMonth,
-      year,
-    };
-  };
-
-  export const getStartTimeFromDate = (startDate: Date): string => {
-    const now = new Date();
-
-    const startTime = startDate.toString().split(" ").at(4)!.slice(0, 5);
-    const { dayOfMonth, month } = getDateAttributes(startDate);
-
-    if (DateUtils.isDifferentDay(now, startDate)) {
-      return `${dayOfMonth} ${month} at ${startTime}`;
-    }
-
-    return startTime;
-  };
-
-  export const getPassedTime = (startDate: Date): string => {
-    const now = new Date();
-    const diff = now.getTime() - startDate.getTime() + 1000;
-    return prettyMs(diff, {
-      unitCount: 2,
-      secondsDecimalDigits: 0,
-    });
-  };
+  // export const formatDuration = (minutes: number): string => {
+  //   const hours = Math.floor(minutes / 60);
+  //   const mins = minutes % 60;
+  //   return `${hours}h ${mins}m`;
+  // };
 
   export const stringToLabelValue = (value: string) => {
     return {
---
+++ application/lib/utils/timeUtils/index.ts
@@ -0,0 +1,58 @@
+import prettyMs from "pretty-ms";
+
+/* eslint-disable @typescript-eslint/no-namespace */
+export namespace TimeUtils {
+  export const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+  const getDayOfMonthFromDate = (date: Date) => {
+    return date.toISOString().split("T")[0].split("-").at(2);
+  };
+
+  export const isDifferentDay = (date1: Date, date2: Date): boolean => {
+    const differenceMoreThanADay =
+      Math.abs(date1.getTime() - date2.getTime()) > oneDayInMilliseconds;
+
+    if (differenceMoreThanADay) {
+      return true;
+    }
+
+    return getDayOfMonthFromDate(date1) !== getDayOfMonthFromDate(date2);
+  };
+
+  export const dateToInputValue = (date: Date): string => {
+    return date.toISOString().split("T")[0];
+  };
+
+  const getDateAttributes = (date: Date) => {
+    const [dayOfWeek, month, dayOfMonth, year] = date.toDateString().split(" ");
+
+    return {
+      dayOfWeek,
+      month,
+      dayOfMonth,
+      year,
+    };
+  };
+
+  export const getStartTimeFromDate = (startDate: Date): string => {
+    const now = new Date();
+
+    const startTime = startDate.toString().split(" ").at(4)!.slice(0, 5);
+    const { dayOfMonth, month } = getDateAttributes(startDate);
+
+    if (TimeUtils.isDifferentDay(now, startDate)) {
+      return `${dayOfMonth} ${month} at ${startTime}`;
+    }
+
+    return startTime;
+  };
+
+  export const getPassedTime = (startDate: Date): string => {
+    const now = new Date();
+    const diff = now.getTime() - startDate.getTime() + 1000;
+    return prettyMs(diff, {
+      unitCount: 2,
+      secondsDecimalDigits: 0,
+    });
+  };
+}
