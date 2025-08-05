828b8b35393b867928c7a52ee48b39eaefcb242d
deadlyunicorn <hello@deadlyunicorn.dev>
2025-08-05 14:36:29 +0300

    added @serwist/next so that we can turn our app into a PWA

--- application/next.config.ts
+++ application/next.config.ts
@@ -1,7 +1,21 @@
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
+
+// Generate a revision for cache busting
+const revision = crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  additionalPrecacheEntries: [
+    { url: "/", revision }, // Precache the root page
+    { url: "/offline", revision }, // Precache the offline fallback page
+  ],
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
